bff27b2ff7ee3faef93f320e70850d2ae86f1847
Pari9678 <vanillap1711@gmail.com>
2026-08-11 19:04:11 +1000

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,12 @@
+print("Binary to Decimal Converter")
+print("----------------------------")
+binary = input("Enter your Binary: ")
+decimal = 0
+power = 0
+
+for digit in binary[::-1]:
+    if digit == "1":
+        decimal = decimal + (2 ** power)
+    power = power + 1
+
+print("Decimal :", decimal)
